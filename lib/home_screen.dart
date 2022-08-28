8fbc91acab655fe20ef6a21a6a84ca97fef35083
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 00:32:25 +0200

    SafeArea if there is no AppBar to show statusBar on mobile

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,30 +5,30 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        leading: Icon(
-          Icons.menu,
-        ),
-        title: Text(
-          'First App'
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.notifications_active,
-            ),
-            onPressed: (){print('notification clicked');},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.account_circle,
-            ),
-            onPressed: (){print('User clicked');},
-          ),
-        ],
-      ),
-      body: Text('Welcom '),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.red,
+      //   leading: Icon(
+      //     Icons.menu,
+      //   ),
+      //   title: Text(
+      //     'First App'
+      //   ),
+      //   actions: [
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.notifications_active,
+      //       ),
+      //       onPressed: (){print('notification clicked');},
+      //     ),
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.account_circle,
+      //       ),
+      //       onPressed: (){print('User clicked');},
+      //     ),
+      //   ],
+      // ),
+      body: SafeArea(child: Text('Welcom ')),
     );
   }
 }
