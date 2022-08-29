242bdd5831f6ca3499bf0cc330b07caf9b1484db
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 16:52:23 +0200

    Email Address Text Form Field

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,12 +9,41 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Login',
               style: TextStyle(
                 fontSize: 40.0,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            TextFormField(
+              //Making keyboard just for Email
+              keyboardType: TextInputType.emailAddress,
+              //After user finishing all email texrt
+              // onFieldSubmitted:(String emailTextValue)
+              // {
+              //   print(emailTextValue);
+              // } ,
+              // //Every char user Enter it
+              // onChanged: (String emailTextValue){
+              //   print(emailTextValue);
+              // },
+              decoration: InputDecoration(
+                //PlsceHolder
+                //hintText: 'Email Address',
+                labelText: 'Email Address',
+                // Email Icon
+                prefixIcon: Icon(
+                  Icons.email,
+                ),
+                //To disappear Border
+               // border: InputBorder.none,
+                border: OutlineInputBorder(),
               ),
             ),
           ],
