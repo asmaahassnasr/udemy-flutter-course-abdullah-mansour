ec8626688bbd2f9cf45dadbd91ac6e91ce250ab0
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 16:30:56 +0200

    1 - Adding login Text

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -6,7 +6,20 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
