ce217e15ec3ae41cbb2383988f2a30f06092a746
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 17:37:07 +0200

    wraping colum widge with scrollView to avoid keyboard overflow on typing

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,106 +11,108 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            TextFormField(
-              controller: emailController,
-              //Making keyboard just for Email
-              keyboardType: TextInputType.emailAddress,
-              //After user finishing all email texrt
-              // onFieldSubmitted:(String emailTextValue)
-              // {
-              //   print(emailTextValue);
-              // } ,
-              // //Every char user Enter it
-              // onChanged: (String emailTextValue){
-              //   print(emailTextValue);
-              // },
-              decoration: InputDecoration(
-                //PlsceHolder
-                //hintText: 'Email Address',
-                labelText: 'Email Address',
-                // Email Icon
-                prefixIcon: Icon(
-                  Icons.email,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                //To disappear Border
-               // border: InputBorder.none,
-                border: OutlineInputBorder(),
               ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            TextFormField(
-              controller: passwordController,
-              //Making keyboard just for password
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: InputDecoration(
-                //PlsceHolder
-                //hintText: 'Email Address',
-                labelText: 'Password',
-                // password Icon
-                prefixIcon: Icon(
-                  Icons.lock,
-                ),
-                suffixIcon: Icon(
-                  Icons.remove_red_eye,
-                ),
-                //To disappear Border
-                // border: InputBorder.none,
-                border: OutlineInputBorder(),
+              SizedBox(
+                height: 50.0,
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Container(
-              width: double.infinity,
-              color: Colors.blue,
-              child: MaterialButton(
-                  onPressed: (){
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
-                  child: Text(
-                  'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+              TextFormField(
+                controller: emailController,
+                //Making keyboard just for Email
+                keyboardType: TextInputType.emailAddress,
+                //After user finishing all email texrt
+                // onFieldSubmitted:(String emailTextValue)
+                // {
+                //   print(emailTextValue);
+                // } ,
+                // //Every char user Enter it
+                // onChanged: (String emailTextValue){
+                //   print(emailTextValue);
+                // },
+                decoration: InputDecoration(
+                  //PlsceHolder
+                  //hintText: 'Email Address',
+                  labelText: 'Email Address',
+                  // Email Icon
+                  prefixIcon: Icon(
+                    Icons.email,
                   ),
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an accout?',
+                  //To disappear Border
+                 // border: InputBorder.none,
+                  border: OutlineInputBorder(),
                 ),
-                TextButton(
-                    onPressed: (){},
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              TextFormField(
+                controller: passwordController,
+                //Making keyboard just for password
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  //PlsceHolder
+                  //hintText: 'Email Address',
+                  labelText: 'Password',
+                  // password Icon
+                  prefixIcon: Icon(
+                    Icons.lock,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.remove_red_eye,
+                  ),
+                  //To disappear Border
+                  // border: InputBorder.none,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: MaterialButton(
+                    onPressed: (){
+                      print(emailController.text);
+                      print(passwordController.text);
+                    },
                     child: Text(
-                      'Register Now'
+                    'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an accout?',
+                  ),
+                  TextButton(
+                      onPressed: (){},
+                      child: Text(
+                        'Register Now'
+                      ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
