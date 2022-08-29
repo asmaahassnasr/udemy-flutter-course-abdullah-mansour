cc2605d5a43a394933754fe9eb7ae065b6467409
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 19:07:40 +0200

    Making AppBar and body full screen white without elevation

--- lib/messenger_screen.dart
+++ lib/messenger_screen.dart
@@ -5,7 +5,11 @@ class MessengerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
+      backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+        ),
     );
   }
 }
