6eece0b51021c85872c8976b8a2d0db86107b462
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-29 19:23:43 +0200

    Creating AppBar icons : actions

--- lib/messenger_screen.dart
+++ lib/messenger_screen.dart
@@ -9,7 +9,53 @@ class MessengerScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0.0,
+          title: Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundImage: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512__340.jpg'
+                ),
+              ),
+              SizedBox(
+                width: 15.0,
+              ),
+              Text(
+                'Chats',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 15.0,
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 16.0,
+                    color: Colors.white,
+                  ),
+                ),
+            ),
+            IconButton(
+              onPressed: (){},
+              icon: CircleAvatar(
+                backgroundColor: Colors.blue,
+                radius: 15.0,
+                child: Icon(
+                  Icons.edit,
+                  size: 16.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
+      
     );
   }
 }
