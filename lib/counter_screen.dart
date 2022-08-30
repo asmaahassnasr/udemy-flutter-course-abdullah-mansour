7b030e51491471c1b33a20dda58901af1aa42c2f
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-30 19:35:12 +0200

    Creating body for counter screen

--- lib/counter_screen.dart
+++ lib/counter_screen.dart
@@ -10,7 +10,38 @@ class CounterScreen extends StatelessWidget {
           'Conter Screen'
         ),
       ),
-      
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: (){},
+              child: Text(
+                'MINUS',
+              ),
+            ),
+            Padding(
+              // To make padding for left and right
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
+              child: Text(
+                '3',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){},
+              child: Text(
+                'PLUS',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
