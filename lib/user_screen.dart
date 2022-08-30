39c95fa57e03b56300341fbadbacd5952fa7f211
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-30 17:15:18 +0200

    Creating list view model and adding item to it

--- lib/user_screen.dart
+++ lib/user_screen.dart
@@ -9,46 +9,19 @@ class UsersScreen extends StatelessWidget {
           'Users',
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 15.0,
-                  child: Text(
-                    '1',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Column(
-                  //To make col and avatar at the same line
-                  mainAxisSize: MainAxisSize.min,
-                  //To make phone num at the start of the screen
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Asmaa Hassan Nasr',
-                      style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '+20106199647',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+      body: ListView.separated(
+        itemBuilder: (context, index)=>buildUserItem(),
+        separatorBuilder: (context , index) => Padding(
+          padding: const EdgeInsetsDirectional.only(
+            start: 20.0,
+          ),
+          child: Container(
+            width: double.infinity,
+            height: 1.0,
+            color: Colors.grey[300],
+          ),
         ),
+        itemCount: 20,
       ),
     );
   }
@@ -57,4 +30,45 @@ class UsersScreen extends StatelessWidget {
   //2. Build List.
   //3. Add Item to list.
 
+  Widget buildUserItem() => Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        CircleAvatar(
+          radius: 15.0,
+          child: Text(
+            '1',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Column(
+          //To make col and avatar at the same line
+          mainAxisSize: MainAxisSize.min,
+          //To make phone num at the start of the screen
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Asmaa Hassan Nasr',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '+20106199647',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
