e5f7a83bb8376fc8913696bc2de5e2c9db565a7d
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-30 17:08:46 +0200

    Creating/ Building Item

--- lib/user_screen.dart
+++ lib/user_screen.dart
@@ -9,6 +9,52 @@ class UsersScreen extends StatelessWidget {
           'Users',
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 15.0,
+                  child: Text(
+                    '1',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Column(
+                  //To make col and avatar at the same line
+                  mainAxisSize: MainAxisSize.min,
+                  //To make phone num at the start of the screen
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Asmaa Hassan Nasr',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '+20106199647',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+        ),
+      ),
     );
   }
+  //To make ListView
+  //1. Build Item.
+  //2. Build List.
+  //3. Add Item to list.
+
 }
