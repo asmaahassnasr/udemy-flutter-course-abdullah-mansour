7026ba336801136e46c1b1799927b1eea0ea94df
Asmaa Hassan <asmaa.has.Nasr@outlook.com>
2022-08-31 21:03:00 +0200

    form Validation

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -4,6 +4,7 @@ class LoginScreen extends StatelessWidget {
 
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,106 +14,121 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  //Making keyboard just for Email
-                  keyboardType: TextInputType.emailAddress,
-                  //After user finishing all email texrt
-                  // onFieldSubmitted:(String emailTextValue)
-                  // {
-                  //   print(emailTextValue);
-                  // } ,
-                  // //Every char user Enter it
-                  // onChanged: (String emailTextValue){
-                  //   print(emailTextValue);
-                  // },
-                  decoration: InputDecoration(
-                    //PlsceHolder
-                    //hintText: 'Email Address',
-                    labelText: 'Email Address',
-                    // Email Icon
-                    prefixIcon: Icon(
-                      Icons.email,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    //To disappear Border
-                    // border: InputBorder.none,
-                    border: OutlineInputBorder(),
                   ),
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  //Making keyboard just for password
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    //PlsceHolder
-                    //hintText: 'Email Address',
-                    labelText: 'Password',
-                    // password Icon
-                    prefixIcon: Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye,
-                    ),
-                    //To disappear Border
-                    // border: InputBorder.none,
-                    border: OutlineInputBorder(),
+                  SizedBox(
+                    height: 50.0,
                   ),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: MaterialButton(
-                    onPressed: (){
-                      print(emailController.text);
-                      print(passwordController.text);
+                  TextFormField(
+                    controller: emailController,
+                    //Making keyboard just for Email
+                    keyboardType: TextInputType.emailAddress,
+                    //After user finishing all email texrt
+                    // onFieldSubmitted:(String emailTextValue)
+                    // {
+                    //   print(emailTextValue);
+                    // } ,
+                    // //Every char user Enter it
+                    // onChanged: (String emailTextValue){
+                    //   print(emailTextValue);
+                    // },
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'Email address is required';
+                      }
                     },
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white,
+                    decoration: InputDecoration(
+                      //PlsceHolder
+                      //hintText: 'Email Address',
+                      labelText: 'Email Address',
+                      // Email Icon
+                      prefixIcon: Icon(
+                        Icons.email,
+                      ),
+                      //To disappear Border
+                      // border: InputBorder.none,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    //Making keyboard just for password
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'Password is required';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      //PlsceHolder
+                      //hintText: 'Email Address',
+                      labelText: 'Password',
+                      // password Icon
+                      prefixIcon: Icon(
+                        Icons.lock,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                      ),
+                      //To disappear Border
+                      // border: InputBorder.none,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    color: Colors.blue,
+                    child: MaterialButton(
+                      onPressed: (){
+                        if(formKey.currentState!.validate()){
+                          print(emailController.text);
+                          print(passwordController.text);
+                        }
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an accout?',
-                    ),
-                    TextButton(
-                      onPressed: (){},
-                      child: Text(
-                          'Register Now'
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an accout?',
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      TextButton(
+                        onPressed: (){},
+                        child: Text(
+                            'Register Now'
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
